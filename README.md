9df1c49b7c476690ae118fb3d1227c18336a8879
Doug Hatcher <doug.hatcher@blueacornici.com>
2022-06-04 19:25:49 -0400

    moves readme

--- README.md
+++ README.md
@@ -12,3 +12,12 @@ Running magento and adobe commerce locally with a cli tool
 [![Go](https://github.com/superterran/mach/actions/workflows/go.yml/badge.svg?branch=main)](https://github.com/superterran/magento-cli/actions/workflows/go.yml) 
 [![Go Report Card](https://goreportcard.com/badge/github.com/superterran/mach)](https://goreportcard.com/report/github.com/superterran/magneto-cli)
 [![codecov](https://codecov.io/gh/superterran/mach/branch/main/graph/badge.svg?token=S48U2MJP9I)](https://codecov.io/gh/superterran/magento-cli)
+
+
+# Running Locally
+
+```bash
+    cd example/
+    composer install
+    go run .. serve
+```
--- cmd/serve.go
+++ cmd/serve.go
@@ -2,6 +2,7 @@
 package cmd
 
 import (
+	"bufio"
 	"fmt"
 	"os/exec"
 
@@ -9,11 +10,11 @@ import (
 	"github.com/spf13/viper"
 )
 
-var Command string = "php -S " + BaseUrl + " -t ./pub/ ./phpserver/router.php"
+var Command = " -S " + BaseUrl + " -t ./pub/ ./phpserver/router.php"
 
 // BuildImageDirname tells the tool which directory to itereate through to find Dockerfiles. defaults the present working
 // directory, but a good practice is to mint a .mach.yaml and set this to `images` or the like when building an IaC repo.
-var BaseUrl string = "magento.test"
+var BaseUrl string = "magento.test:8200"
 
 var serveCmd = CreateServeCmd()
 
@@ -35,7 +36,7 @@ func init() {
 
 	serveCmd.Flags().StringVar(&BaseUrl, "base-url", BaseUrl, "base url for php server")
 	viper.SetDefault("BaseUrl", BaseUrl)
-	viper.BindPFlag("BuildImageDirname", serveCmd.Flags().Lookup("base-url"))
+	viper.BindPFlag("base-url", serveCmd.Flags().Lookup("base-url"))
 
 }
 
@@ -50,7 +51,29 @@ func runServe(cmd *cobra.Command, args []string) error {
 // MainServeFlow will run the local php server
 func MainServeFlow(args []string) error {
 
-	fmt.Println(Command)
-	exec.Command(Command)
+	cmd := exec.Command("php", "-S ", BaseUrl, "-t", "./pub/", "./phpserver/router.php")
+	// some command output will be input into stderr
+	// e.g.
+	// cmd := exec.Command("../../bin/master_build")
+	// stderr, err := cmd.StderrPipe()
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	err = cmd.Start()
+	fmt.Println("The command is running")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// print the output of the subprocess
+	scanner := bufio.NewScanner(stdout)
+	for scanner.Scan() {
+		m := scanner.Text()
+		fmt.Println(m)
+	}
+	cmd.Wait()
 	return nil
+
 }
